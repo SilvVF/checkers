ce60d1360ebfeb0ef1f734b2d69366b02eb11ef7
David Silver <david.silver232@gmail.com>
2023-05-17 09:41:37 -0400

    move validation for single jumps and no promotions

--- app/src/main/java/io/silv/checkers/MainActivity.kt
+++ app/src/main/java/io/silv/checkers/MainActivity.kt
@@ -8,127 +8,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import io.ktor.client.HttpClient
-import io.silv.checkers.ui.Blue
 import io.silv.checkers.ui.CheckerBoard
 import io.silv.checkers.ui.Empty
-import io.silv.checkers.ui.Piece
-import io.silv.checkers.ui.Red
-import io.silv.checkers.ui.dragdrop.Cord
 import io.silv.checkers.ui.dragdrop.generateInitialBoard
 import io.silv.checkers.ui.theme.DragDropTestTheme
-import java.sql.Driver
-import kotlin.math.abs
-
-sealed interface Direction
-
-sealed interface XYDirection: Direction {
-    object UpLeft: XYDirection
-    object DownLeft: XYDirection
-    object UpRight: XYDirection
-    object DownRight: XYDirection
-    object None: XYDirection
-}
-
-fun getXyDirection(x: XDirection, y: YDirection): XYDirection {
-    return when (x) {
-        XDirection.Left -> {
-            when (y) {
-                YDirection.Up -> XYDirection.UpLeft
-                YDirection.Down -> XYDirection.DownLeft
-                else -> XYDirection.None
-            }
-        }
-        XDirection.Right -> {
-            when (y) {
-                YDirection.Up -> XYDirection.UpRight
-                YDirection.Down -> XYDirection.DownRight
-                else -> XYDirection.None
-            }
-        }
-        else -> XYDirection.None
-    }
-}
-
-sealed interface YDirection: Direction {
-    object Up: YDirection
-    object Down: YDirection
-    object None: YDirection
-}
-
-sealed interface XDirection: Direction {
-    object Left: XDirection
-    object Right: XDirection
-    object None: XDirection
-}
-
-fun getYDirection(difY: Int): YDirection {
-    return when {
-        difY > 0 -> {
-            YDirection.Up
-        }
-        difY < 0 -> {
-            YDirection.Down
-        }
-        else -> YDirection.None
-    }
-}
-
-fun getXDirection(difX: Int): XDirection {
-    return when  {
-        difX < 0 -> { // right
-            XDirection.Right
-        }
-        difX > 0 -> { // left
-            XDirection.Left
-        }
-        else -> XDirection.None
-    }
-}
-
-fun Int.isNeg() = this < 0
-
-
-data class MoveResult(
-    val valid: Boolean,
-    val captured: List<Cord>
-)
-
-fun validatePlacement(board: List<List<Piece>>, from: Cord, to: Cord, piece: Piece): MoveResult {
-    val piece = board[from.first][from.second]
-    val bad = MoveResult(valid = false, emptyList())
-    val difX = from.second - to.second
-    val difY = from.first - to.first
-
-    val distY = abs(difY)
-    val distX = abs(difX)
-
-    val xDirection = getXDirection(difX)
-    val yDirection = getYDirection(difY)
-    val xyDirection = getXyDirection(xDirection, yDirection)
-
-    // make sure move is not retreating from opponent
-    if (
-        (piece is Red && yDirection != YDirection.Down) ||
-        (piece is Blue && yDirection != YDirection.Up)
-    ) {
-        return bad
-    }
-    // moving to non empty square
-    if (board[to.first][to.second] !is Empty) {
-        return bad
-    }
-    // moving diagonal 1 in correct direction and to is empty
-    if (xyDirection == XYDirection.None) {
-        return bad
-    }
-    if (distY == 1 && distX == 1) {
-        return MoveResult(true, emptyList())
-    }
-
-
-    return MoveResult(true, emptyList())
-}
+import io.silv.checkers.validation.validatePlacement
 
 
 class MainActivity : ComponentActivity() {
@@ -150,7 +34,7 @@ class MainActivity : ComponentActivity() {
                 CheckerBoard(
                     board = board,
                     onDropAction = { fromCord, toCord, piece ->
-                        val result = validatePlacement(board, fromCord, toCord, piece)
+                        val result = validatePlacement(board, fromCord, toCord)
                         if (!result.valid) {
                             return@CheckerBoard
                         }
---
+++ app/src/main/java/io/silv/checkers/validation/MoveValidation.kt
@@ -0,0 +1,187 @@
+package io.silv.checkers.validation
+
+import io.silv.checkers.ui.Blue
+import io.silv.checkers.ui.Empty
+import io.silv.checkers.ui.Piece
+import io.silv.checkers.ui.Red
+import io.silv.checkers.ui.dragdrop.Cord
+import kotlin.math.abs
+
+sealed interface Direction
+
+sealed interface XYDirection: Direction {
+    object UpLeft: XYDirection
+    object DownLeft: XYDirection
+    object UpRight: XYDirection
+    object DownRight: XYDirection
+    object None: XYDirection
+}
+
+sealed interface YDirection: Direction {
+    object Up: YDirection
+    object Down: YDirection
+    object None: YDirection
+}
+
+sealed interface XDirection: Direction {
+    object Left: XDirection
+    object Right: XDirection
+    object None: XDirection
+}
+
+fun getXyDirection(x: XDirection, y: YDirection): XYDirection {
+    return when (x) {
+        XDirection.Left -> {
+            when (y) {
+                YDirection.Up -> XYDirection.UpLeft
+                YDirection.Down -> XYDirection.DownLeft
+                else -> XYDirection.None
+            }
+        }
+        XDirection.Right -> {
+            when (y) {
+                YDirection.Up -> XYDirection.UpRight
+                YDirection.Down -> XYDirection.DownRight
+                else -> XYDirection.None
+            }
+        }
+        else -> XYDirection.None
+    }
+}
+
+fun getDiagonalCord(from: Cord, xyDirection: XYDirection): Cord? {
+    return runCatching {
+        when (xyDirection) {
+            XYDirection.DownLeft -> from.first + 1 to from.second - 1
+            XYDirection.DownRight -> from.first + 1 to from.second + 1
+            XYDirection.UpLeft -> from.first - 1 to from.second - 1
+            XYDirection.UpRight -> from.first - 1 to from.second + 1
+            XYDirection.None -> null
+        }
+    }.getOrNull()
+}
+
+fun List<List<Piece>>.getDiagonal(from: Cord, xyDirection: XYDirection): Piece? {
+    return when (xyDirection) {
+        XYDirection.DownLeft -> this
+            .getOrNull(from.first + 1)
+            ?.getOrNull(from.second - 1)
+        XYDirection.DownRight -> this
+            .getOrNull(from.first + 1)
+            ?.getOrNull(from.second + 1)
+        XYDirection.UpLeft -> this
+            .getOrNull(from.first - 1)
+            ?.getOrNull(from.second - 1)
+        XYDirection.UpRight -> this
+            .getOrNull(from.first - 1)
+            ?.getOrNull(from.second + 1)
+        XYDirection.None -> null
+    }
+}
+
+fun getYDirection(difY: Int): YDirection {
+    return when {
+        difY > 0 -> {
+            YDirection.Up
+        }
+        difY < 0 -> {
+            YDirection.Down
+        }
+        else -> YDirection.None
+    }
+}
+
+fun getXDirection(difX: Int): XDirection {
+    return when  {
+        difX < 0 -> { // right
+            XDirection.Right
+        }
+        difX > 0 -> { // left
+            XDirection.Left
+        }
+        else -> XDirection.None
+    }
+}
+
+data class MoveResult(
+    val valid: Boolean,
+    val captured: List<Cord>
+)
+
+/**
+ * When this function is called To cord is assumed to be 2 spaces away relative to the From Cord
+ * and in the direction passed as XYDirection.
+ */
+fun validateJump(
+    board: List<List<Piece>>,
+    from: Cord,
+    piece: Piece,
+    direction: XYDirection
+): Boolean {
+    return when (piece) {
+        is Red -> {
+            when (direction) {
+                XYDirection.DownRight -> board.getDiagonal(from, XYDirection.DownRight) is Blue
+                XYDirection.DownLeft -> board.getDiagonal(from, XYDirection.DownLeft) is Blue
+                else -> false
+            }
+        }
+        is Blue -> {
+            when (direction) {
+                XYDirection.UpLeft ->  board.getDiagonal(from, XYDirection.UpLeft) is Red
+                XYDirection.UpRight ->  board.getDiagonal(from, XYDirection.UpRight) is Red
+                else -> false
+            }
+        }
+        else -> false
+    }
+}
+
+fun validatePlacement(board: List<List<Piece>>, from: Cord, to: Cord): MoveResult {
+    val piece = board[from.first][from.second]
+    val bad = MoveResult(valid = false, emptyList())
+    val difX = from.second - to.second
+    val difY = from.first - to.first
+
+    val distY = abs(difY)
+    val distX = abs(difX)
+
+    val xDirection = getXDirection(difX)
+    val yDirection = getYDirection(difY)
+    val xyDirection = getXyDirection(xDirection, yDirection)
+
+    // make sure move is not retreating from opponent
+    if (
+        (piece is Red && yDirection != YDirection.Down) ||
+        (piece is Blue && yDirection != YDirection.Up)
+    ) {
+        return bad
+    }
+    // moving to non empty square
+    if (board[to.first][to.second] !is Empty) {
+        return bad
+    }
+    if (xyDirection == XYDirection.None) {
+        return bad
+    }
+
+    // moving diagonal 1 in correct direction and to is empty
+    getDiagonalCord(from, xyDirection)?.let { cord ->
+        return when {
+            distX == 1 && distY == 1 -> {
+                MoveResult(true, emptyList())
+            }
+            distX == 2 && distY == 2 -> {
+                if (validateJump(board, from, piece, xyDirection)) {
+                    MoveResult(true, listOf(cord))
+                } else {
+                    bad
+                }
+            }
+            else -> { // moved distance greater than one hop which is invalid
+                bad
+            }
+        }
+    }
+    return bad
+}
