3cf5b3013afe595d193595024636d75622167433
David Silver <david.silver232@gmail.com>
2023-05-24 14:51:59 -0400

    defer some functions of the ai move improve speed

--- app/src/main/java/io/silv/checkers/usecase/AiOponnent.kt
+++ app/src/main/java/io/silv/checkers/usecase/AiOponnent.kt
@@ -1,23 +1,23 @@
 package io.silv.checkers.usecase
 
-import android.util.Log
 import io.silv.checkers.Blue
 import io.silv.checkers.Cord
 import io.silv.checkers.Empty
 import io.silv.checkers.Piece
 import io.silv.checkers.Red
-import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.withContext
 
 
-suspend fun aiMove(board: List<List<Piece>>) = minimax(board, 3, true)
+suspend fun aiMove(board: List<List<Piece>>): Pair<Double, List<List<Piece>>> {
+    return minimax(board, 3, true)
+}
 
 
-fun minimax(
+
+suspend fun minimax(
     board: List<List<Piece>>,
     depth: Int,
     maxPlayer: Boolean,
@@ -28,18 +28,18 @@ fun minimax(
     if (maxPlayer) {
         var value = Double.MIN_VALUE
         var bestBoard: List<List<Piece>> = board
-        for (b in getAllBoards(board, Red())) {
+        for (b in getAllBoardsDeferred(board, Red())) {
             val (v) = minimax(b, depth - 1, false)
-            if (v >= value) {
+            if (v <= value) {
                 value = v
                 bestBoard = b
             }
         }
-       return value to bestBoard
+        return value to bestBoard
     } else {
         var value = Double.MAX_VALUE
         var bestBoard: List<List<Piece>> = board
-        for (b in getAllBoards(board, Blue())) {
+        for (b in getAllBoardsDeferred(board, Blue())) {
             val (v) = minimax(b, depth - 1, true)
             if (v <= value) {
                 value = v
@@ -90,65 +90,66 @@ fun simulateMove(from: Cord, toC: Cord, skip: Cord?, board: List<List<Piece>>): 
     }
 }
 
-fun getAllBoards(board: List<List<Piece>>, piece: Piece):  List<List<List<Piece>>> {
-    return getAllPieces(board, piece).flatMap { piece ->
-        validMoves(board, piece).map { (move, skip) ->
+suspend fun getAllBoards(board: List<List<Piece>>, piece: Piece):  List<List<List<Piece>>> {
+    return getAllPieces(board, piece).flatMap { p ->
+        validMoves(board, p).map { (move, skip) ->
             val (from, to) = move
             simulateMove(from, to, skip, board)
         }
     }
 }
 
+suspend fun getAllBoardsDeferred(board: List<List<Piece>>, piece: Piece) = withContext(Dispatchers.IO) {
+    getAllPieces(board, piece).map { piece ->
+        async {
+            validMoves(board, piece).map { (move, skip) ->
+                val (from, to) = move
+                simulateMove(from, to, skip, board)
+            }
+        }
+    }
+        .awaitAll()
+        .flatten()
+}
 
-fun validMoves(board: List<List<Piece>>, piece: Piece): List<Pair<Pair<Cord, Cord>, Cord?>> {
+
+fun validMoves(board: List<List<Piece>>, piece: Piece): List<Pair<Pair<Cord, Cord>, Cord?>>  {
     val blueDirections = listOf(XYDirection.UpLeft, XYDirection.UpRight)
     val redDirections = listOf(XYDirection.DownRight ,XYDirection.DownLeft)
     val directions = blueDirections + redDirections
+    val blue = piece.value == Blue().value
 
-    fun getValidJumps(
-        blue: Boolean
-    ): List<Pair<Pair<Cord, Cord>, Cord?>> =
-        buildList {
-            board.forEachIndexed { i, row ->
-                row.forEachIndexed { j, piece ->
-                    if (blue && piece is Blue || !blue && piece is Red) {
-                        directions.forEach {
-                            if (validateJump(board, i to j,board[i][j], it)){
-                                val skip = getDiagonalCord(i to j, it)!!
-                                add(
-                                    i to j to getDiagonalCord(skip, it)!! to skip
-                                )
+    val getValidJumps = buildList {
+        board.forEachIndexed { i, row ->
+            row.forEachIndexed { j, piece ->
+                if (blue && piece is Blue || !blue && piece is Red) {
+                    directions.forEach {
+                        if (validateJump(board, i to j, board[i][j], it)) {
+                            val skip = getDiagonalCord(i to j, it)!!
+                            add(i to j to getDiagonalCord(skip, it)!! to skip)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    val getMoveNoJump = buildList {
+        board.forEachIndexed { i, row ->
+            row.forEachIndexed { j, piece ->
+                if (blue && piece is Blue || !blue && piece is Red) {
+                    directions.mapNotNull { getDiagonalCord(i to j, it) }.forEach {
+                        runCatching {
+                            if (validatePlacement(board, i to j, it).valid) {
+                                add((i to j) to it to null)
                             }
                         }
                     }
                 }
             }
         }
-
-    fun getMovesNoJump(
-        blue: Boolean
-    ): List<Pair<Pair<Cord, Cord>, Cord?>> =
-        buildList {
-            board.forEachIndexed { i, row ->
-                row.forEachIndexed { j, piece ->
-                    if (blue && piece is Blue || !blue && piece is Red) {
-                        directions.mapNotNull { getDiagonalCord(i to j, it) }.forEach {
-                            runCatching {
-                                if(validatePlacement(board, i to j, it).valid) {
-                                    add(
-                                        (i to j) to it to null
-                                    )
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-    return getMovesNoJump(
-        piece.value == Blue().value
-    ) + getValidJumps(
-        piece.value == Blue().value
-    )
+    }
+    return getMoveNoJump + getValidJumps
 }
